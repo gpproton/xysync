e948bc31a3b75cd9b35542d140125a13c7f9285f
Godwin peter .O <me@godwin.dev>
2020-04-03 08:32:43 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"github.com/urfave/cli/v2"
+	"log"
+	"os"
 )
 
 func main() {
 
-	//app := &cli.App{
-	//	Flags: []cli.Flag{
-	//		&cli.StringFlag{
-	//			Name: "config",
-	//			Aliases: []string{"c"},
-	//			Usage: "Load configuration from `FILE`",
-	//		},
-	//	},
-	//}
-	//
-	//err := app.Run(os.Args)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name: "config",
+				Aliases: []string{"c"},
+				Usage: "Load configuration from `FILE`",
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 
 }
