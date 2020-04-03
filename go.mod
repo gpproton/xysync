c167ce2fd5c85857ff1effa0c6c7e11468149a99
Godwin peter .O <me@godwin.dev>
2020-04-03 05:32:27 +0100

    clean up

--- go.mod
+++ go.mod
@@ -2,14 +2,4 @@ module github.com/my/repo
 
 go 1.14
 
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
-	github.com/go-redis/redis/v7 v7.2.0 // indirect
-	github.com/jinzhu/gorm v1.9.12
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/kr/pretty v0.2.0
-	github.com/kr/text v0.2.0 // indirect
-	github.com/sirupsen/logrus v1.5.0 // indirect
-	github.com/urfave/cli/v2 v2.2.0
-	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
-)
+require ( )
--- src/main.go
+++ src/main.go
@@ -1,50 +1,25 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/urfave/cli/v2"
-
-	_ "github.com/kr/pretty"
-
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
-	// _ "github.com/jinzhu/gorm/dialects/postgres"
-	// _ "github.com/jinzhu/gorm/dialects/sqlite"
-	// _ "github.com/jinzhu/gorm/dialects/mssql"
 
 )
 
 func main() {
-	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "config",
-				Aliases: []string{"c"},
-				Usage:   "Load configuration from `FILE`",
-			},
-		},
-	}
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//var testcard []string
-	//db, err := gorm.Open("postgres", "host=localhost sslmode=disable port=5432 user=postgres dbname=testDatabase password=")
-	//if err != nil {
-	//	panic(err)
+	//app := &cli.App{
+	//	Flags: []cli.Flag{
+	//		&cli.StringFlag{
+	//			Name: "config",
+	//			Aliases: []string{"c"},
+	//			Usage: "Load configuration from `FILE`",
+	//		},
+	//	},
 	//}
-	//db.LogMode(true)
-	//// db.Raw("SELECT * FROM tc_positions where sync != 1").Find(&testcard)
 	//
-	//query := db.Raw("SELECT * FROM tc_positions where sync != 1").Find(&testcard)
-	//m := query.Value
-	//fmt.Printf("%v", m)
-	//
-	//fmt.Println("---- TEST OUTPUT ----")
-	//pretty.Print(testcard)
-	//defer db.Close()
+	//err := app.Run(os.Args)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
 
 }
