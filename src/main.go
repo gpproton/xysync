1e418f77f7e5eb9afdb2012c40ecf6ad307bb661
Godwin peter .O <me@godwin.dev>
2020-04-03 09:25:07 +0100

    Test cron

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/robfig/cron/v3"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
@@ -11,9 +13,9 @@ func main() {
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name: "config",
+				Name:    "config",
 				Aliases: []string{"c"},
-				Usage: "Load configuration from `FILE`",
+				Usage:   "Load configuration from `FILE`",
 			},
 		},
 	}
@@ -24,4 +26,17 @@ func main() {
 	}
 
 
+	done := make(chan bool)
+
+	func() {
+		c := cron.New()
+		c.AddFunc("@every 5s", func() {fmt.Println("First", 1) })
+		c.AddFunc("@every 2s", func() {fmt.Println("First", 2) })
+		c.Start()
+		done <- true
+	}()
+
+
 }
+
+
